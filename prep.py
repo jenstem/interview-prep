41c5eec07430591fd1123dc8d3a42e0041e07799
jenstem <jennifer.stemkowski@gmail.com>
2024-09-19 10:37:53 -0400

    Convert array to a list with the same elements using tolist() method

--- prep.py
+++ prep.py
@@ -1,71 +1,10 @@
 from array import array
-import numpy as np
-
-# Create an array and traverse
-
-my_array = np.array('i', [1,2,3,4,5,6,7,8,9])
-
-# my_list = [10,11,12]
-
-# def traverse_array():
-#     for i in my_array:
-#         print(i)
-
-# traverse_array()
 
 
-# Access individual elements through indexes
-# print(my_array[3])
+my_array = array('i', [1,2,3,4,5,6,7,8,9])
 
-# Append any value to the array using append() method
 
-# my_array.append(10)
-# print(my_array)
+# Convert array to a list with the same elements using tolist() method
 
-# Insert value in an array using insert() method
-
-# my_array.insert(9, 10)
-# print(my_array)
-
-# Extend python array using extend() method
-
-# my_array.extend([10,11,12])
-# print(my_array)
-
-# Add items from list into array using fromlist() method
-
-# my_array.fromlist(my_list)
-# print(my_array)
-
-# Remove any array element using remove() method
-
-# my_array.remove(9)
-# print(my_array)
-
-# Remove last array element using pop() method
-
-# my_array.pop()
-# print(my_array)
-
-# Fetch any element through its index using index() method
-
-# index = my_array.index(5)
-# print("The index of 5 is:", index)
-
-# Reverse a python array using reverse() method
-
-# my_array.reverse()
-# print(my_array)
-
-# Get array buffer information through buffer_info() method
-
-# print(my_array.buffer_info())
-
-# Check for number of occurrences of an element using count() method
-
-# print(my_array.count(8))
-
-# Convert array to string using str() method
-
-my_string = str(my_array)
-print(my_string)
+my_list = my_array.tolist()
+print(my_list)
