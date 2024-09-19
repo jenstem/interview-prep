9214c5dbf55876bef69333a0e385703ab4f47e75
jenstem <jennifer.stemkowski@gmail.com>
2024-09-19 10:50:17 -0400

    Slice elements from an array

--- prep.py
+++ prep.py
@@ -1,10 +1,6 @@
-from array import array
+my_array = [1,2,3,4,5,6,7,8,9,10]
 
 
-my_array = array('i', [1,2,3,4,5,6,7,8,9])
+# Slice elements from an array
 
-
-# Convert array to a list with the same elements using tolist() method
-
-my_list = my_array.tolist()
-print(my_list)
+print(my_array[:5])
