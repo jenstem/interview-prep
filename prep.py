08416707a0cb4c077b6e656c47d9e8e49db4a0d6
jenstem <jennifer.stemkowski@gmail.com>
2024-10-02 14:16:00 +0100

    Create class SingleLinkedList to initialize head + tail

--- prep.py
+++ prep.py
@@ -4,3 +4,9 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
