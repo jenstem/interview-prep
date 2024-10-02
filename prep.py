357ef3b33761eb2e69ac9c0ed4f0352c16d8a5c4
jenstem <jennifer.stemkowski@gmail.com>
2024-10-02 14:22:00 +0100

    Use __str__ method to print result string

--- prep.py
+++ prep.py
@@ -5,8 +5,20 @@ class Node:
         self.value = value
         self.next = None
 
+
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
+
+
+    def __str__(self):
+        temp_node = self.head
+        result = ''
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += ' -> '
+            temp_node = temp_node.next
+        return result
