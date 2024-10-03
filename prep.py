55ecdf225529bf5c0624fb29a1bedab8b3bf835a
jenstem <jennifer.stemkowski@gmail.com>
2024-10-03 17:49:39 -0400

    Create append method for Singly Linked List

--- prep.py
+++ prep.py
@@ -24,3 +24,24 @@ class SinglyLinkedList:
                 result += ' -> '
             temp_node = temp_node.next
         return result
+
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+
+new_linked_list = SinglyLinkedList()
+new_linked_list.append(10)
+new_linked_list.append(20)
+new_linked_list.append(30)
+new_linked_list.append(40)
+print(new_linked_list.length)
+print(new_linked_list.head.value)
+print(new_linked_list)
