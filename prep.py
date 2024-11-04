00a6e985df4b3810172935c1fcb4f113104fed95
jenstem <jennifer.stemkowski@gmail.com>
2024-11-04 14:46:17 -0500

    Create delete_node method for Circular Singly Linked List

--- prep.py
+++ prep.py
@@ -41,6 +41,30 @@ class CSLinkedList:
         return count
 
 
+    def delete_node(self, key):
+        '''
+        Deletes a node from the linked list
+        '''
+        if self.head.data == key:
+            cur = self.head
+            while cur.next != self.head:
+                cur = cur.next
+            if self.head == self.head.next:
+                self.head = None
+            else:
+                cur.next = self.head.next
+                self.head = cur.next
+        else:
+            cur = self.head
+            prev = None
+            while cur.next != self.head:
+                prev = cur
+                cur = cur.next
+                if cur.data == key:
+                    prev.next = cur.next
+                    cur = cur.next
+
+
     def prepend(self, data):
         '''
         Adds a new node to the beginning of the linked list
