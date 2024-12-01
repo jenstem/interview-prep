a29f0452dc68bd8f1e7c88530e2211c04cc3fa89
jenstem <jennifer.stemkowski@gmail.com>
2024-12-01 14:01:00 +0100

    Create append method for Circular Doubly Linked List

--- prep.py
+++ prep.py
@@ -12,19 +12,36 @@ class Node:
 
 
 class CircularDoublyLinkedList:
-    # def __init__(self):
-    #     self.head = None
-    #     self.tail = None
-    #     self.length = 0
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    def __init__(self, value):
+
+    def append(self, value):
         new_node = Node(value)
-        new_node.next = new_node
-        new_node.prev = new_node
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+        else:
+            self.tail.next = new_node
+            self.head.prev = new_node
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.tail = new_node
+        self.length += 1
 
 
-new_cdll = CircularDoublyLinkedList(10)
-print(new_cdll.head.value)
+
+
+
+new_cdll = CircularDoublyLinkedList()
+new_cdll.append(10)
+new_cdll.append(20)
+new_cdll.append(30)
+new_cdll.append(40)
+print(new_cdll.tail.value)
+print(new_cdll.length)
