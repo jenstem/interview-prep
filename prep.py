dfcb90709865bb25720d2b361eae1e78f8254fff
jenstem <jennifer.stemkowski@gmail.com>
2024-12-11 12:13:49 -0500

    Solve remove duplicates in unsorted Linked List

--- prep.py
+++ prep.py
@@ -76,6 +76,27 @@ class LinkedList:
         return self
 
 
+    def remove_duplicates(ll):
+        if ll.head is None:
+            return
+
+        current_node = ll.head
+        prev_node = None
+
+        while current_node:
+            runner = current_node
+            while runner.next:
+                if runner.next.value == current_node.value:
+                    runner.next = runner.next.next
+                else:
+                    runner = runner.next
+            prev_node = current_node
+            current_node = current_node.next
+        ll.tail = prev_node
+        return ll.head
+
+
 customLL = LinkedList()
 customLL.generate(10, 0, 99)
 print(customLL)
+print(len(customLL))
