6a3d225770068c1fac9493087450f8b991a92757
jenstem <jennifer.stemkowski@gmail.com>
2024-12-14 07:21:00 +0100

    Learn pop method for Stacks

--- prep.py
+++ prep.py
@@ -1,5 +1,12 @@
-# Create a stack
-
 customStack()
-
 customStack = []
+
+
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+customStack.push(4)
+
+
+customStack = [1,2,3,4]
+customStack.pop()
