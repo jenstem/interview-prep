f73c4b00d3916e3800fd4193f63a242a7c934780
jenstem <jennifer.stemkowski@gmail.com>
2024-12-17 09:47:14 -0500

    Create isEmpty method for Stack w a List

--- prep.py
+++ prep.py
@@ -1,12 +1,18 @@
 # Create a Stack with a List
 # No size limit
 
-
 class Stack:
     def __init__(self):
         self.list = []
 
-
     def __str__(self):
         values = [str(x) for x in reversed(self.list)]
         return '\n'.join(values)
+
+    def isEmpty(self):
+        return len(self.list) == 0
+
+
+
+customStack = Stack()
+print(customStack.isEmpty())
