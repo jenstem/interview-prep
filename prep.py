46882f60dea0a75b876017c47d82de6b299f668c
jenstem <jennifer.stemkowski@gmail.com>
2024-12-19 10:52:57 -0500

    Create class Stack + init method for Stack w size limits

--- prep.py
+++ prep.py
@@ -1,63 +1,13 @@
 # Create a Stack with a List
-# No size limit
+# With size limit
 
 class Stack:
     """
     A class to represent a Stack data structure.
     """
-    def __init__(self):
+    def __init__(self, maxSize):
         """
         Initialize the stack with an empty list.
         """
+        self.maxSize = maxSize
         self.list = []
-
-    def __str__(self):
-        """
-        Return a string representation of the stack.
-        """
-        values = [str(x) for x in reversed(self.list)]
-        return '\n'.join(values)
-
-    def isEmpty(self):
-        """
-        Check if the stack is empty.
-        """
-        return len(self.list) == 0
-
-    def push(self, value):
-        """
-        Add an element to the top of the stack.
-        """
-        self.list.append(value)
-        return "The element has been successfully inserted"
-
-    def pop(self):
-        """
-        Remove and return the top element of the stack.
-        """
-        if self.isEmpty():
-            return "The stack is empty"
-        else:
-            return self.list.pop()
-
-    def peek(self):
-        """
-        Return the top element of the stack without removing it.
-        """
-        if self.isEmpty():
-            return "The stack is empty"
-        else:
-            return self.list[len(self.list)-1]
-
-    def delete(self):
-        """
-        Delete the stack.
-        """
-        self.list = None
-
-
-customStack = Stack()
-customStack.push(1)
-customStack.push(2)
-customStack.push(3)
-print(customStack.delete())
