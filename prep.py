eb12c3a6e9bf9b309d7aa9f95e6326d74b3ab552
jenstem <jennifer.stemkowski@gmail.com>
2024-12-29 14:09:00 +0100

    Create class Queue, init method, str method for Queue w Python List No Size Limit

--- prep.py
+++ prep.py
@@ -1,62 +1,10 @@
-# Create a Stack with a Linked List
+# Create a Queue with a Python List - no size limit
 
-class Node:
-    def __init__(self, value = None):
-        self.value = value
-        self.next = None
 
-class LinkedList:
+class Queue:
     def __init__(self):
-        self.head = None
-
-    def __iter__(self):
-        curNode = self.head
-        while curNode:
-            yield curNode
-            curNode = curNode.next
-
-class Stack:
-    def __init__(self):
-        self.LinkedList = LinkedList()
+        self.items = []
 
     def __str__(self):
-        values = [str(x.value) for x in self.LinkedList]
-        return '\n'.join(values)
-
-    def isEmpty(self):
-        if self.LinkedList.head == None:
-            return True
-        else:
-            return False
-
-    def push(self, value):
-        node = Node(value)
-        node.next = self.LinkedList.head
-        self.LinkedList.head = node
-
-    def pop(self):
-        if self.isEmpty():
-            return "There are no elements in the stack"
-        else:
-            nodeValue = self.LinkedList.head.value
-            self.LinkedList.head = self.LinkedList.head.next
-            return nodeValue
-
-    def peek(self):
-        if self.isEmpty():
-            return "There are no elements in the stack"
-        else:
-            nodeValue = self.LinkedList.head.value
-            return nodeValue
-
-    def delete(self):
-        self.LinkedList.head = None
-
-
-
-customStack = Stack()
-customStack.push(1)
-customStack.push(2)
-customStack.push(3)
-print(customStack.peek())
-print(customStack)
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
