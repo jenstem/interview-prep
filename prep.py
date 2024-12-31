29e80dace0055090c2d80daf5911952df66d1eac
jenstem <jennifer.stemkowski@gmail.com>
2024-12-31 10:16:03 -0500

    Create str method for Circular Queue w Python List + Fixed Size Limit

--- prep.py
+++ prep.py
@@ -7,3 +7,7 @@ class Queue:
         self.maxSize = maxSize
         self.start = -1
         self.top = -1
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
