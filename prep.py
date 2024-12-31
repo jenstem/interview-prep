74c79ae3c109aa0e5f43a0ee74fc71fbbfaf32ef
jenstem <jennifer.stemkowski@gmail.com>
2024-12-31 10:15:14 -0500

    Create class Queue + init method for Circular Queue w Python List + Fixed Size Limit

--- prep.py
+++ prep.py
@@ -1,42 +1,9 @@
-# Create a Queue with a Python List - no size limit
+# Create a Circular Queue with a Python List - Fixed Size limit
 
 
 class Queue:
-    def __init__(self):
-        self.items = []
-
-    def __str__(self):
-        values = [str(x) for x in self.items]
-        return ' '.join(values)
-
-    def isEmpty(self):
-        if self.items == []:
-            return True
-        else:
-            return False
-
-    def enqueue(self, value):
-        self.items.append(value)
-        return "The element is inserted at the end of the queue"
-
-    def dequeue(self):
-        if self.isEmpty():
-            return "The queue is empty"
-        else:
-            return self.items.pop(0)
-
-    def peek(self):
-        if self.isEmpty():
-            return "The queue is empty"
-        else:
-            return self.items[0]
-
-    def delete(self):
-        self.items = None
-
-customQueue = Queue()
-customQueue.enqueue(1)
-customQueue.enqueue(2)
-customQueue.enqueue(3)
-print(customQueue.dequeue())
-print(customQueue)
+    def __init__(self, maxSize):
+        self.items = maxSize * [None]
+        self.maxSize = maxSize
+        self.start = -1
+        self.top = -1
