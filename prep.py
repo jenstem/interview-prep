b57953a30fc2d5993909378c3289a53d941a62c0
jenstem <jennifer.stemkowski@gmail.com>
2025-01-12 14:03:00 +0100

    Create enqueue method for Animal Shelter Problem

--- prep.py
+++ prep.py
@@ -14,3 +14,9 @@ class AnimalShelter():
     def __init__(self):
         self.cats = []
         self.dogs = []
+
+    def enqueue(self, animal, type):
+        if type == 'Cat':
+            self.cats.append(animal)
+        else:
+            self.dogs.append(animal)
