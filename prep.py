b15a5faacec3da20cd1b91897ca78048c14d06d6
jenstem <jennifer.stemkowski@gmail.com>
2025-01-15 10:53:32 -0500

    Breakdown example of recursion

--- prep.py
+++ prep.py
@@ -1,54 +1,18 @@
-# Stack and Queue Interview Questions
+# Recursion
 
-# Animal Shelter
+def firstMethod():
+    secondMethod()
+    print("I am the first method")
 
-# An animal shelter, which holds only dogs and cats, operates on a strictly
-# "first in, first out" basis.  People must adopt either the "oldest" (based on
-# arrival time) of all animals at the shelter, or they can select whether they
-# would prefer a dog or a cat (and will receive the oldest animal of that type).
-# They cannot select which specific animal they would like.  Create the data
-# structures to maintain this system and implement operations such as enqueue,
-# dequeueAny, dequeueDog, and dequeueCat.
+def secondMethod():
+    thirdMethod()
+    print("I am the second method")
 
-class AnimalShelter():
-    def __init__(self):
-        self.cats = []
-        self.dogs = []
+def thirdMethod():
+    fourthMethod()
+    print("I am the third method")
 
-    def enqueue(self, animal, type):
-        if type == 'Cat':
-            self.cats.append(animal)
-        else:
-            self.dogs.append(animal)
+def fourthMethod():
+    print("I am the fourth method")
 
-    def dequeueCat(self):
-        if len(self.cats) == 0:
-            return None
-        else:
-            cat = self.cats.pop(0)
-            return cat
-
-    def dequeueDog(self):
-        if len(self.dogs) == 0:
-            return None
-        else:
-            dog = self.dogs.pop(0)
-            return dog
-
-    def dequeueAny(self):
-        if len(self.cats) == 0:
-            result = self.dogs.pop(0)
-        else:
-            result = self.cats.pop(0)
-        return result
-
-
-customQueue = AnimalShelter()
-customQueue.enqueue('Cat1', 'Cat')
-customQueue.enqueue('Cat2', 'Cat')
-customQueue.enqueue('Dog1', 'Dog')
-customQueue.enqueue('Cat3', 'Cat')
-customQueue.enqueue('Dog2', 'Dog')
-print(customQueue.dequeueCat())
-print(customQueue.dequeueDog())
-print(customQueue.dequeueAny())
+firstMethod()
