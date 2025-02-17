513045f2c420dc1a459064e23bff9e5565d2700e
jenstem <jennifer.stemkowski@gmail.com>
2025-02-17 15:14:06 -0500

    Solve fib algorithm problem

--- prep.py
+++ prep.py
@@ -1,11 +1,19 @@
-# Recursion - recursiveRange
+# Recursion - fib
 
-# Write a function called recursiveRange which accepts a number
-# and adds up all the numbers from 0 to the number passed to the function
+# Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of whole numbers 0,1, 1, 2, 3, 5, 8, ... which starts with 0 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
 
-def recursiveRange(num):
-    if num == 0:
-        return 0
-    return num + recursiveRange(num - 1)
+# Examples
 
-print(recursiveRange(10))
+# fib(4) # 3
+# fib(10) # 55
+# fib(28) # 317811
+# fib(35) # 9227465
+
+def fib(num):
+    assert num >= 0 and int(num) == num, 'The number must be a positive integer!'
+    if num in [0,1]:
+        return num
+    else:
+        return fib(num-1) + fib(num-2)
+
+print(fib(7))
