daa1094e1e708a156bd2a8d8a49fe655b8fa1c38
jenstem <jennifer.stemkowski@gmail.com>
2025-02-18 15:18:28 -0500

    Solve capitalizeFirst algorithm using recursion

--- prep.py
+++ prep.py
@@ -1,25 +1,14 @@
-# Recursion - someRecursive
+# Recursion - capitalizeFirst
 
-# Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
+# Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
 
-# Examples
+# Example
 
-# someRecursive([1,2,3,4], isOdd) # true
-# someRecursive([4,6,8,9], isOdd) # true
-# someRecursive([4,6,8], isOdd) # false
+# capitalizeFirst(['car', 'taco', 'banana']) # ['Car','Taco','Banana']
 
-def isOdd(num):
-    if num % 2 != 0:
-        return True
-    else:
-        return False    
-
-def someRecursive(arr, cb):
+def capitalizeFirst(arr):
+    result = []
     if len(arr) == 0:
-        return False
-    if cb(arr[0]):
-        return True
-    return someRecursive(arr[1:], cb)
-
-print(someRecursive([1,2,3,4], isOdd))   
-print(someRecursive([4,6,8], isOdd)) 
+        return result
+    result.append(arr[0][0].upper() + arr[0][1:])
+    return result + capitalizeFirst(arr[1:])
