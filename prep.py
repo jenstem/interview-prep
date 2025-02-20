abba18e21afac15fd43b63d0de173a56e2baa9b8
jenstem <jennifer.stemkowski@gmail.com>
2025-02-20 10:18:55 -0500

    Create Node class + init method for General Tree

--- prep.py
+++ prep.py
@@ -1,32 +1,7 @@
-# Recursion - collectStrings
+# Create a General Tree
 
-# Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string.
-
-# Examples
-
-# obj = {
-#   "stuff": 'foo',
-#   "data": {
-#     "val": {
-#       "thing": {
-#         "info": 'bar',
-#         "moreInfo": {
-#           "evenMoreInfo": {
-#             "weMadeIt": 'baz'
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
- 
-# collectStrings(obj) # ['foo', 'bar', 'baz']
-
-def collectStrings(obj):
-    resultArr = []
-    for key in obj:
-        if type(obj[key]) is str:
-            resultArr.append(obj[key])
-        elif type(obj[key]) is dict:
-            resultArr.extend(collectStrings(obj[key]))
-    return resultArr
+class TreeNode:
+    def __init__(self, data, children = []):
+        self.data = data
+        self.children = children
+   
