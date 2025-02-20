1b3a03bb5043624cbdec1a5f5b70f4e58c21d6e2
jenstem <jennifer.stemkowski@gmail.com>
2025-02-20 13:20:58 -0500

    Create preOrderTraversal function for Binary Tree w Linked List

--- prep.py
+++ prep.py
@@ -10,4 +10,13 @@ newBT = TreeNode("Drinks")
 leftChild = TreeNode("Hot")
 rightChild = TreeNode("Cold") 
 newBT.leftChild = leftChild
-newBT.rightChild = rightChild      
+newBT.rightChild = rightChild
+
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)    
+
+preOrderTraversal(newBT)
