17aca2f9d8b4be947d7a52105a15674bab85d86d
jenstem <jennifer.stemkowski@gmail.com>
2025-02-26 10:16:18 -0500

    Create searchBT function to search for certain noce value using queue

--- prep.py
+++ prep.py
@@ -53,6 +53,25 @@ def levelOrderTraversal(rootNode):
             if (root.value.rightChild is not None):
                 customQueue.enqueue(root.value.rightChild)
 
+def searchBT(rootNode, nodeValue):
+    if not rootNode:
+        return "The BT does not exist"
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            if root.value.data == nodeValue:
+                return "Success"
+            if (root.value.leftChild is not None):
+                customQueue.enqueue(root.value.leftChild)
+            if (root.value.rightChild is not None):
+                customQueue.enqueue(root.value.rightChild)
+        return "Not found"
+
+
 preOrderTraversal(newBT)
 inOrderTraversal(newBT)
 postOrderTraversal(newBT)
+levelOrderTraversal(newBT)
+print(searchBT(newBT, "Tea"))
