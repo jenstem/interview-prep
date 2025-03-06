5be4878ae1b3b79551078663c40140a65975712a
jenstem <jennifer.stemkowski@gmail.com>
2025-03-06 14:25:12 -0500

    Create insertNode function to insert a node into a Binary Search Tree

--- prep.py
+++ prep.py
@@ -6,4 +6,25 @@ class BSTNode:
         self.leftChild = None
         self.rightChild = None
 
-newBST = BSTNode(None)    
+    def insertNode(rootNode, nodeValue):
+        if rootNode.data == None:
+            rootNode.data = nodeValue
+        elif nodeValue <= rootNode.data:
+            if rootNode.leftChild == None:
+                rootNode.leftChild = BSTNode(nodeValue)
+            else:
+                BSTNode.insertNode(rootNode.leftChild, nodeValue)
+        else:
+            if rootNode.rightChild == None:
+                rootNode.rightChild = BSTNode(nodeValue)
+            else:
+                BSTNode.insertNode(rootNode.rightChild, nodeValue)
+        return "The node has been successfully inserted"     
+
+newBST = BSTNode(None)
+print(newBST.insertNode(newBST, 70))
+print(newBST.insertNode(newBST, 60))
+print(newBST.data)
+print(newBST.leftChild.data)
+print(newBST.rightChild.data)
+
