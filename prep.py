5e9e2422b9f5b66d6836bd84f5ab6408446d7b83
jenstem <jennifer.stemkowski@gmail.com>
2025-03-18 12:05:37 -0400

    Create AVL Tree

--- prep.py
+++ prep.py
@@ -1,122 +1,12 @@
-# Binary Search Tree
+# AVL Trees
 
 import QueueLinkedList as queue
 
-class BSTNode:
+class AVLNode:
     def __init__(self, data):
         self.data = data
         self.leftChild = None
         self.rightChild = None
+        self.height = 1
 
-def insertNode(rootNode, nodeValue):
-    if rootNode.data == None:
-        rootNode.data = nodeValue
-    elif nodeValue <= rootNode.data:
-        if rootNode.leftChild is None:
-            rootNode.leftChild = BSTNode(nodeValue)
-        else:
-            insertNode(rootNode.leftChild, nodeValue)
-    else:
-        if rootNode.rightChild is None:
-            rootNode.rightChild = BSTNode(nodeValue)
-        else:
-            insertNode(rootNode.rightChild, nodeValue)
-    return "The node has been successfully inserted"
-
-def preOrderTraversal(rootNode):
-    if not rootNode:
-        return
-    print(rootNode.data)
-    preOrderTraversal(rootNode.leftChild)
-    preOrderTraversal(rootNode.rightChild)
-
-def inOrderTraversal(rootNode):
-    if not rootNode:
-        return
-    inOrderTraversal(rootNode.leftChild)
-    print(rootNode.data)
-    inOrderTraversal(rootNode.rightChild)
-
-def postOrderTraversal(rootNode):
-    if not rootNode:
-        return
-    postOrderTraversal(rootNode.leftChild)
-    postOrderTraversal(rootNode.rightChild)
-    print(rootNode.data)
-
-def levelOrderTraversal(rootNode):
-    if not rootNode:
-        return
-    else:
-        customQueue = queue.Queue()
-        customQueue.enqueue(rootNode)
-        while not(customQueue.isEmpty()):
-            root = customQueue.dequeue()
-            print(root.value.data)
-            if root.value.leftChild is not None:
-                customQueue.enqueue(root.value.leftChild)
-            if root.value.rightChild is not None:
-                customQueue.enqueue(root.value.rightChild)    
-
-def searchNode(rootNode, nodeValue):
-    if rootNode.data == nodeValue:
-        print("The value is found")
-    elif nodeValue < rootNode.data:
-        if rootNode.leftChild.data == nodeValue:
-            print("The value is found")
-        else:
-            searchNode(rootNode.leftChild, nodeValue)
-    else:
-        if rootNode.rightChild.data == nodeValue:
-            print("The value is found")
-        else:
-            searchNode(rootNode.rightChild, nodeValue)
-
-def minValue(bstNode):
-    current = bstNode
-    while current.leftChild is not None:
-        current = current.leftChild
-    return current.data
-
-def deleteNode(rootNode, nodeValue):
-    if rootNode is None:
-        return rootNode
-    if nodeValue < rootNode.data:
-        rootNode.leftChild = deleteNode(rootNode.leftChild, nodeValue)   
-    elif nodeValue > rootNode.data:
-        rootNode.rightChild = deleteNode(rootNode.rightChild, nodeValue)
-    else:
-        if rootNode.leftChild is None:
-            temp = rootNode.rightChild
-            rootNode = None
-            return temp
-        if rootNode.rightChild is None:
-            temp = rootNode.leftChild
-            rootNode = None
-            return temp
-        temp = minValue(rootNode.rightChild)
-        rootNode.data = temp
-        rootNode.rightChild = deleteNode(rootNode.rightChild, temp) 
-    return rootNode
-
-def deleteBST(rootNode):
-    rootNode.data = None
-    rootNode.leftChild = None
-    rootNode.rightChild = None
-    return "The BST has been successfully deleted"
-
-
-newBST = BSTNode(None)
-print(insertNode(newBST, 70))
-print(insertNode(newBST, 50))
-print(insertNode(newBST, 90))
-print(insertNode(newBST, 30))
-print(insertNode(newBST, 60))
-print(insertNode(newBST, 80))
-print(insertNode(newBST, 100))
-print(insertNode(newBST, 20))
-print(insertNode(newBST, 40))
-deleteBST(newBST)
-levelOrderTraversal(newBST)
-
-
+newAVL = AVLNode()        
