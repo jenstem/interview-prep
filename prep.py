d074dfe328b951868048178a65954953961b9304
jenstem <jennifer.stemkowski@gmail.com>
2025-03-19 13:49:09 -0400

    Correct function indentation + Create searchNode function to search for specific node value in AVL Tree

--- prep.py
+++ prep.py
@@ -9,40 +9,55 @@ class AVLNode:
         self.rightChild = None
         self.height = 1
 
-    def preOrderTraversal(rootNode):
-        if not rootNode:
-            return
-        print(rootNode.data)
-        preOrderTraversal(rootNode.leftChild)
-        preOrderTraversal(rootNode.rightChild)   
+def preOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftChild)
+    preOrderTraversal(rootNode.rightChild)   
 
-    def inOrderTraversal(rootNode):
-        if not rootNode:
-            return
-        inOrderTraversal(rootNode.leftChild)
-        print(rootNode.data)
-        inOrderTraversal(rootNode.rightChild)    
+def inOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftChild)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild)
 
-    def postOrderTraversal(rootNode):
-        if not rootNode:
-            return
-        postOrderTraversal(rootNode.leftChild)
-        postOrderTraversal(rootNode.rightChild)
-        print(rootNode.data)  
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
 
-    def levelOrderTraversal(rootNode):
-        if not rootNode:
-            return
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            print(root.value.data)
+            if root.value.leftChild is not None:
+                customQueue.enqueue(root.value.leftChild)
+            if root.value.rightChild is not None:
+                customQueue.enqueue(root.value.rightChild)
+
+def searchNode(rootNode, nodeValue):
+    if rootNode.data == nodeValue:
+        print("The value is found")
+    elif nodeValue < rootNode.data:
+        if rootNode.leftChild.data == nodeValue:
+            print("The value is found")
         else:
-            customQueue = queue.Queue()
-            customQueue.enqueue(rootNode)
-            while not(customQueue.isEmpty()):
-                root = customQueue.dequeue()
-                print(root.value.data)
-                if root.value.leftChild is not None:
-                    customQueue.enqueue(root.value.leftChild)
-                if root.value.rightChild is not None:
-                    customQueue.enqueue(root.value.rightChild)       
+            searchNode(rootNode.leftChild, nodeValue)
+    else:
+        if rootNode.rightChild.data == nodeValue:
+            print("The value is found")
+        else:
+            searchNode(rootNode.rightChild, nodeValue)
 
 newAVL = AVLNode()
+newAVL.searchNode(10)
 newAVL.levelOrderTraversal()        
