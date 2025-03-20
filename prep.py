ae88ec6b071138a72ab71da686a6b21c51ac1195
jenstem <jennifer.stemkowski@gmail.com>
2025-03-20 11:58:13 -0400

    Create getBalance function for AVL Tree

--- prep.py
+++ prep.py
@@ -78,6 +78,11 @@ def leftRotate(disbalanceNode):
     disbalanceNode.height = 1 + max(getHeight(disbalanceNode.leftChild), getHeight(disbalanceNode.rightChild))
     newRoot.height = 1 + max(getHeight(newRoot.leftChild), getHeight(newRoot.rightChild))
     return newRoot
+
+def getBalance(rootNode):
+    if not rootNode:
+        return 0
+    return getHeight(rootNode.leftChild) - getHeight(rootNode.rightChild)
             
 
 newAVL = AVLNode()
