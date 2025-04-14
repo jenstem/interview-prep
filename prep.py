c3079975bc5a04bc4d443c185aabbdc0f1ca163a
jenstem <jennifer.stemkowski@gmail.com>
2025-04-14 15:44:18 -0400

    Create searchString method for Trie

--- prep.py
+++ prep.py
@@ -17,10 +17,25 @@ class Trie:
             if node is None:
                 node = TrieNode()
                 currentNode.children.update({ch:node})   
-            current = node
-        current.endOfString = True
+            currentNode = node
+        currentNode.endOfString = True
         print("Successfully inserted!")    
 
+    def searchString(self, word):
+        currentNode = self.root
+        for i in word:
+            node = currentNode.children.get(i)
+            if node == None:
+                return False 
+            currentNode = node
+
+        if currentNode.endOfString == True:
+            return True
+        else:
+            return False    
+
+
 newTrie = Trie()
-newTrie.insertString("App")
-newTrie.insertString("Appl")
+newTrie.insertString('App')
+newTrie.insertString('Appl')
+print(newTrie.searchString('App'))
