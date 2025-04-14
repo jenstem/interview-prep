24735af243d2b3330e6db6eb6fdf461b7ec7c8ba
jenstem <jennifer.stemkowski@gmail.com>
2025-04-14 15:16:11 -0400

    Create insertString method for Trie

--- prep.py
+++ prep.py
@@ -9,4 +9,18 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
 
+    def insertString(self, word):
+        currentNode = self.root
+        for i in word:
+            ch = i
+            node = currentNode.children.get(ch)
+            if node is None:
+                node = TrieNode()
+                currentNode.children.update({ch:node})   
+            current = node
+        current.endOfString = True
+        print("Successfully inserted!")    
+
 newTrie = Trie()
+newTrie.insertString("App")
+newTrie.insertString("Appl")
