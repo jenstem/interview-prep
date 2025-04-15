785adaf49265d7c193f36f8bada2e34b86757a46
jenstem <jennifer.stemkowski@gmail.com>
2025-04-15 15:23:28 -0400

    Create deleteString method for Trie

--- prep.py
+++ prep.py
@@ -34,8 +34,37 @@ class Trie:
         else:
             return False    
 
+def deleteString(root, word, index):
+    ch = word[index]
+    currentNode = root.children.get(ch)
+    canThisNodeBeDeleted = False
+
+    if len(currentNode.children) > 1:
+        deleteString(currentNode, word, index + 1)
+        return False
+
+    if index == len(word) - 1:
+        if len(currentNode.children) >= 1:
+            currentNode.endOfString = False
+            return False
+        else:
+            root.children.pop(ch)
+            return True
+
+    if currentNode.endOfString == True:
+        deleteString(currentNode, word, index + 1)
+        return False
+
+    canThisNodeBeDeleted = deleteString(currentNode, word, index + 1)
+    if canThisNodeBeDeleted == True:
+        root.children.pop(ch)
+        return True
+    else:
+        return False
+
 
 newTrie = Trie()
 newTrie.insertString('App')
 newTrie.insertString('Appl')
+deleteString(newTrie.root, 'App', 0)
 print(newTrie.searchString('App'))
