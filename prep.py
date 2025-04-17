c61fe2d5bf6b94d92b84f029f22ab0cc1679e56c
jenstem <jennifer.stemkowski@gmail.com>
2025-04-17 10:35:25 -0400

    Create modASCII function for Hashing to convert strings from user

--- prep.py
+++ prep.py
@@ -4,4 +4,15 @@ def mod(number, cellNumber):
     return number % cellNumber
 
 mod(400, 24)    
-mod(700, 24)    
+mod(700, 24)   
+
+
+# ASCII function
+
+def modASCII(string, cellNumber):
+    total = 0
+    for i in string:
+        total += ord(i)
+    return total % cellNumber
+
+modASCII("ABC", 24) 
