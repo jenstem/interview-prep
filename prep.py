e1c15d03e17959d83794ea5ac9be1dac7a34cb9c
jenstem <jennifer.stemkowski@gmail.com>
2025-04-30 15:21:50 -0400

    Create insertionSort method for Insertion Sort

--- prep.py
+++ prep.py
@@ -1,15 +1,17 @@
 # Sorting
 
-# Selection Sort
+# Insertion Sort
 
-def selectionSort(customList):
-    for i in range(len(customList)):
-        min_index = i
-        for j in range(i + 1, len(customList)):
-            if customList[min_index] > customList[j]:
-                min_index = j
-        customList[i], customList[min_index] = customList[min_index], customList[i]
-    print(customList)    
+def insertionSort(customList):
+    for i in range(1, len(customList)):
+        key = customList[i]
+        j = i - 1
+        while j >= 0 and key < customList[j]:
+            customList[j + 1] = customList[j]
+            j -= 1
+        customList[j + 1] = key
+
+    print(customList)
 
 cList = [2,1,7,6,5,3,4,9,8]
-selectionSort(cList)    
+insertionSort(cList)   
