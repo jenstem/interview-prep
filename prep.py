b9f8cb00e7fb3030d1bdba6e6dbe82919f82f661
jenstem <jennifer.stemkowski@gmail.com>
2025-04-30 16:12:37 -0400

    Create insertionSort method for use on Floating Point Numbers

--- prep.py
+++ prep.py
@@ -1,25 +1,21 @@
 # Sorting
 
-# Insertion Sort with Shift Count
+# Insertion Sort with Floating Point Numbers
 
-# Write a Python program to use insertion sort 
-# to sort a list of numbers and count the number of shifts required.
+# Write a Python function to perform insertion sort 
+# on a list of floating-point numbers and then verify the sorted order.
 
 
 def insertionSort(customList):
-    shiftCount = 0
     for i in range(1, len(customList)):
         key = customList[i]
         j = i - 1
         while j >= 0 and key < customList[j]:
             customList[j + 1] = customList[j]
             j -= 1
-            shiftCount += 1
         customList[j + 1] = key
 
-    return customList, shiftCount
+    return customList
 
 cList = [2,1,7,6,5,3,4,9,8]
-sorted_numbers, shifts = insertionSort(cList)
-print("Sorted List:", sorted_numbers)
-print("Number of Shifts:", shifts)
+print(insertionSort(cList))
