2e0c4da8568bc403a567dfcfb9f09f754501936f
jenstem <jennifer.stemkowski@gmail.com>
2025-05-07 16:01:47 -0400

    Create bucketSort method for Sorting with negative numbers

--- prep.py
+++ prep.py
@@ -4,38 +4,27 @@
 
 import math
 
-def insertionSort(customList):
-    for i in range(1, len(customList)):
-        key = customList[i]
-        j = i - 1
-        while j >= 0 and key < customList[j]:
-            customList[j + 1] = customList[j]
-            j -= 1
-        customList[j + 1] = key
-    return customList
-
 def bucketSort(customList):
-    numberOfBuckets = round(math.sqrt(len(customList)))   
+    numberofBuckets = round(math.sqrt(len(customList)))
+    minValue = min(customList)
     maxValue = max(customList)
-    arr = []
+    rangeVal = (maxValue - minValue) / numberofBuckets
 
-    for i in range(numberOfBuckets):
-        arr.append([])
+    buckets = [[] for _ in range(numberofBuckets)]
 
     for j in customList:
-        index_b = math.ceil(j * numberOfBuckets / maxValue)
-        arr[index_b - 1].append(j)
+        if j == maxValue:
+            buckets[-1].append(j)
+        else:
+            index_b = math.floor((j - minValue) / rangeVal)
+            buckets[index_b].append(j)
 
-    for i in range(numberOfBuckets):
-        arr[i] = insertionSort(arr[i])    
-  
-    k = 0
-    
-    for i in range(numberOfBuckets):
-        for j in range(len(arr[i])):
-            customList[k] = arr[i][j]
-            k += 1
-    return customList
+    sorted_array = []
+    for i in range(numberofBuckets):
+        buckets[i].sort()
+        sorted_array += buckets[i]
+
+    return sorted_array
 
 cList = [2,1,7,6,5,3,4,9,8] 
 print(bucketSort(cList))   
