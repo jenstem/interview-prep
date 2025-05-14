010aa638ea350d234189e4e2fc69d666dd93a6b7
jenstem <jennifer.stemkowski@gmail.com>
2025-05-14 09:36:24 -0400

    Create mergeSort method for Merge Sorting

--- prep.py
+++ prep.py
@@ -70,7 +70,14 @@ def merge(customList, l, m, r):
         j += 1
         k += 1
 
+def mergeSort(customList, l, r):
+    if l < r:
+        m = (l + (r - 1)) // 2
+        mergeSort(customList, l, m)
+        mergeSort(customList, m + 1, r)
+        merge(customList, l, m, r)
+    return customList
 
 
 cList = [2,1,7,6,5,3,4,9,8] 
-print(bucketSort(cList))     
+print(mergeSort(cList, 0, 8))     
