fea07a59c42c4cc9cd9c8c32bd5c03bb048604c4
jenstem <jennifer.stemkowski@gmail.com>
2025-05-20 11:40:03 -0400

    Create swap function for pivot function for Sorting

--- prep.py
+++ prep.py
@@ -1,54 +1,6 @@
 # Sorting
 
-# Merge Sort
+# Pivot Function
 
-# Write a Python function to perform merge sort recursively 
-# and then verify its stability on a list with duplicate values.
-
-def mergeSort(customList):
-    if len(customList) > 1:
-        m = len(customList) // 2
-
-        L = customList[:m]
-        R = customList[m:]
-
-        mergeSort(L)
-        mergeSort(R)
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                customList[k] = L[i]
-                i += 1
-            else:
-                customList[k] = R[j]
-                j += 1
-            k += 1
-
-        while i < len(L):
-            customList[k] = L[i]
-            i += 1
-            k += 1
-
-        while j < len(R):
-            customList[k] = R[j]
-            j += 1
-            k += 1 
-        
-def is_stable(original, sorted_arr):
-    org_indices = {value: [] for value in original}
-    for index, value in enumerate(original):
-        org_indices[value].append(index)
-
-    sorted_indices = []
-    for value in sorted_arr:
-        sorted_indices.append(org_indices[value].pop(0))
-
-    return sorted_indices == sorted(sorted_indices)     
-
-customList = [38, 27, 43, 38, 9, 82, 10, 27]
-sorted_arr = customList.copy()
-mergeSort(sorted_arr)
-print("Sorted array is:", sorted_arr)
-print("Is stable?", is_stable(customList, sorted_arr))
+def swap(my_list, index1, index2):
+    my_list[index1], my_list[index2] = my_list[index2], my_list[index1]
