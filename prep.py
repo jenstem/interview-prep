4cfdad330d58d4513adff14014567441cb35106b
jenstem <jennifer.stemkowski@gmail.com>
2025-05-21 15:41:58 -0400

    Create heapSort method for sorting

--- prep.py
+++ prep.py
@@ -16,3 +16,18 @@ def heapify(customList, n, i):
     if smallest != i:
         customList[i], customList[smallest] = customList[smallest], customList[i]
         heapify(customList, n, smallest)
+
+def heapSort(customList):
+    n = len(customList)
+
+    for i in range(int(n / 2) - 1, -1, -1):
+        heapify(customList, n, i)
+
+    for i in range(n - 1, 0, -1):
+        customList[i], customList[0] = customList[0], customList[i]
+        heapify(customList, i, 0)
+    customList.reverse()    
+
+cList = [2,1,7,6,5,3,4,9,8]
+heapSort(cList)
+print(cList)
