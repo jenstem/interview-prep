e025d422c95ff966164eba665002e50ac7a087b1
jenstem <jennifer.stemkowski@gmail.com>
2025-05-21 15:59:30 -0400

    Solve count comparisions for heapSort method

--- prep.py
+++ prep.py
@@ -19,17 +19,24 @@ def heapify(customList, n, i):
 
 def heapSort(customList):
     n = len(customList)
+    comparisions = 0
 
     for i in range(int(n / 2) - 1, -1, -1):
+        comparisions += 1
         heapify(customList, n, i)
 
     for i in range(n - 1, 0, -1):
         customList[i], customList[0] = customList[0], customList[i]
         heapify(customList, i, 0)
-    customList.reverse()    
+    customList.reverse()
+    return comparisions    
+
+def sort_and_count(customList):
+    comparisions = heapSort(customList)
+    return customList, comparisions    
 
 if __name__ == "__main__":
     data = [12, 11, 13, 5, 6, 7]
-    print("Original array:", data)
-    heapSort(data)
-    print("Sorted array:", data)
+    sortedList, comparisions = sort_and_count(data)
+    print("Sorted array is", sortedList)
+    print("Number of comparisions:", comparisions)
