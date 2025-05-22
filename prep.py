656ab021c372de4eca19453cfb263e1acec2fa94
jenstem <jennifer.stemkowski@gmail.com>
2025-05-22 15:14:07 -0400

    Create heapSort method to sort tuples w/ keys

--- prep.py
+++ prep.py
@@ -2,41 +2,33 @@
 
 # Heap Sort
 
-def heapify(customList, n, i):
-    smallest = i
+def heapify(customList, n, i, key):
+    largest = i
     l = 2 * i + 1
     r = 2 * i + 2
 
-    if l < n and customList[l] < customList[smallest]:
-        smallest = l
+    if l < n and customList[l][key] < customList[largest][key]:
+        largest = l
 
-    if r < n and customList[r] < customList[smallest]:
-        smallest = r     
+    if r < n and customList[r][key] < customList[largest][key]:
+        largest = r     
  
-    if smallest != i:
-        customList[i], customList[smallest] = customList[smallest], customList[i]
-        heapify(customList, n, smallest)
+    if largest != i:
+        customList[i], customList[largest] = customList[largest], customList[i]
+        heapify(customList, n, largest, key)
 
-def heapSort(customList):
+def heapSort(customList, key):
     n = len(customList)
-    comparisions = 0
 
     for i in range(int(n / 2) - 1, -1, -1):
-        comparisions += 1
-        heapify(customList, n, i)
+        heapify(customList, n, i, key)
 
     for i in range(n - 1, 0, -1):
         customList[i], customList[0] = customList[0], customList[i]
-        heapify(customList, i, 0)
-    customList.reverse()
-    return comparisions    
-
-def sort_and_count(customList):
-    comparisions = heapSort(customList)
-    return customList, comparisions    
+        heapify(customList, i, 0, key)
+  
 
 if __name__ == "__main__":
-    data = [12, 11, 13, 5, 6, 7]
-    sortedList, comparisions = sort_and_count(data)
-    print("Sorted array is", sortedList)
-    print("Number of comparisions:", comparisions)
+    data = [(1, 'apple'), (3, 'banana'), (2, 'cherry')]
+    heapSort(data, 0)
+    print(data)
