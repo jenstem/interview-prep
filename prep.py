253e517e0df83a8e026c6ba5d72b03159572f31b
jenstem <jennifer.stemkowski@gmail.com>
2025-05-27 10:25:39 -0400

    Create a binary search method that uses recursion

--- prep.py
+++ prep.py
@@ -4,21 +4,19 @@
 
 import math
 
-def binarySearch(array, value):
-    start = 0
-    end = len(array) - 1
-    middle = math.floor((start + end) // 2)
-    while not(array[middle]==value) and start <= end:
-        if value < array[middle]:
-            end = middle - 1
-        else:
-            start = middle + 1
-        middle = math.floor((start + end) // 2)
+def binarySearch(array, value, start=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    if start > end:
+        return -1
+    middle = (start + end) // 2
     if array[middle] == value:
         return middle
+    elif value < array[middle]:
+        return binarySearch(array, value, start, middle - 1)
     else:
-        return -1        
+        return binarySearch(array, value, middle + 1, end)     
  
 
-custArray = ["A", "B", "D", "E", "J", "K", "L", "Y", "Z"]
-print(binarySearch(custArray, "J"))
+custArray = [8, 9, 12, 15, 17, 19, 20, 21, 28]
+print(binarySearch(custArray, 12))
