31c91bb323463e8d5aa15ed48bf55bbde5d63a26
jenstem <jennifer.stemkowski@gmail.com>
2025-05-27 10:16:34 -0400

    Create binary search method

--- prep.py
+++ prep.py
@@ -1,21 +1,24 @@
 # Searching
 
-# Linear Search
+# Binary Search  
 
-def linearSearch(arr, target):
-    n = len(arr)
-    halfsies = n // 2
+import math
 
-    for i in range(halfsies):
-        if arr[i] == target:
-            return i
-    return -1             
+def binarySearch(array, value):
+    start = 0
+    end = len(array) - 1
+    middle = math.floor((start + end) // 2)
+    while not(array[middle]==value) and start <= end:
+        if value < array[middle]:
+            end = middle - 1
+        else:
+            start = middle + 1
+        middle = math.floor((start + end) // 2)
+    if array[middle] == value:
+        return middle
+    else:
+        return -1        
+ 
 
-arr = [50, 40, 30, 100, 90, 80]
-target = 40
-result = linearSearch(arr, target)
-
-if result != -1:
-    print(f"Element found at index: {result}")
-else:
-    print(f"Element not found in first half of array")    
+custArray = [8, 9, 12, 15, 17, 19, 20, 21, 28]
+print(binarySearch(custArray, 30))
