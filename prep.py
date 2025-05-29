2d19a786a64d12976f2c17385aa97f8179a1232d
jenstem <jennifer.stemkowski@gmail.com>
2025-05-29 11:48:02 -0400

    Create Graph class + addVertex method

--- prep.py
+++ prep.py
@@ -1,23 +1,24 @@
 # Graph
 
-# Dictionary Implementation
+# Adjancency List
 
 class Graph:
-    def __init__(self, gdict=None):
-        if gdict is None:
-            gdict = {}
-        self.gdict = gdict    
+    def __init__(self):
+        self.adjacency_list = {}   
 
-    def addEdge(self, vertex, edge):
-        self.gdict[vertex].append(edge)
-   
-customDict = { "a" : ["b", "c"],
-               "b" : ["a", "d", "e"],
-               "c" : ["a", "e"],
-               "d" : ["b", "e", "f"],
-               "e" : ["c", "d", "f"],
-               "f" : ["d", "e"] }
+    def addVertex(self, vertex):
+        if vertex not in self.adjacency_list.keys():
+            self.adjacency_list[vertex] = []
+            return True 
+        return False
+
+
+customDict = { "a" : ["b", "c", "d"],
+               "b" : ["a", "e"],
+               "c" : ["a", "d"],
+               "d" : ["a", "c", "e"],
+               "e" : ["b", "d"]}
 
 graph = Graph(customDict)
-graph.addEdge("e", "c")
-print(graph.gdict["e"])
+graph.addVertex("e", "c")
+print(graph.adjacency_list["e"])
