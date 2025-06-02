5ca1d14130287b170370915d6e2bbc174ebf210e
jenstem <jennifer.stemkowski@gmail.com>
2025-06-02 17:49:30 -0400

    Create Breadth First Search for Graph Traversal

--- prep.py
+++ prep.py
@@ -1,56 +1,18 @@
 # Graph
 
-# Adjancency List
+# Graph Traversal - BFS
 
-class Graph:
-    def __init__(self):
-        self.adjacency_list = {}   
+def bfs(self, vertex):
+    visited = set()
+    visited.add(vertex)
+    queue = [vertex]
+    while queue:
+        current_vertex = queue.pop(0)
+        print(current_vertex)
+        for adjacent_vertex in self.adjacency_list[current_vertex]:
+            if adjacent_vertex not in visited:
+                visited.add(adjacent_vertex)
+                queue.append(adjacent_vertex)
 
-    def addVertex(self, vertex):
-        if vertex not in self.adjacency_list.keys():
-            self.adjacency_list[vertex] = []
-            return True 
-        return False
 
-    def print_graph(self):
-        for vertex in self.adjacency_list:
-            print(vertex, ":", self.adjacency_list[vertex])
 
-    def addEdge(self, vertex1, vertex2):
-        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
-            self.adjacency_list[vertex1].append(vertex2)
-            self.adjacency_list[vertex2].append(vertex1)
-            return True
-        return False
-
-    def remove_edge(self, vertex1, vertex2):
-        if vertex1 in self.adjacency_list.keys() or vertex2 in self.adjacency_list.keys():
-            try:
-                self.adjacency_list[vertex1].remove(vertex2)
-                self.adjacency_list[vertex2].remove(vertex1)
-            except ValueError:
-                pass
-            return True
-        return False
-    
-    def remove_vertex(self, vertex):
-        if vertex in self.adjacency_list.keys():
-            for other_vertex in self.adjacency_list[vertex]:
-                self.adjacency_list[other_vertex].remove(vertex)
-            del self.adjacency_list[vertex]
-            return True
-        return False
-
-custom_graph = Graph()
-custom_graph.addVertex("A")
-custom_graph.addVertex("B")
-custom_graph.addVertex("C")
-custom_graph.addVertex("D")
-custom_graph.addVertex("E")
-custom_graph.addEdge("A", "B")
-custom_graph.addEdge("A", "C")
-custom_graph.addEdge("B", "D")
-custom_graph.addEdge("C", "D")
-custom_graph.addEdge("D", "E")
-custom_graph.remove_vertex("E")
-custom_graph.print_graph()
